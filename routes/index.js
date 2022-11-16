494ae28862ab78b65bb1a6677c256a3c4f7f3e19
radoneje <53825282+radoneje@users.noreply.github.com>
2022-11-16 13:07:12 +0300

    ddd

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/startStream', function(req, res, next) {
+  console.log("startStream", req.body)
+  res.json("ok")
+});
 
 module.exports = router;
