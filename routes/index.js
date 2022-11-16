1a263c0dd6e23f46ce67e230eb930c36c47b656c
radoneje <53825282+radoneje@users.noreply.github.com>
2022-11-16 13:20:55 +0300

    ddd

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/startStream', function(req, res, next) {
+router.post('/startStream', async function(req, res, next) {
   console.log("startStream", req.body)
-  /*  app: 'live',
-  flashver: 'FMLE/3.0 (compatible; vMix/25.0',
-  swfurl: '',
-  tcurl: 'rtmp://rtmp.rustv.ru:1935/live',
-  pageurl: '',
-  addr: '217.67.180.18',
-  clientid: '1',
-  call: 'publish',
-  name: '1',
-  type: 'live'
-   */
+  let streams = await req.knex("t_22_streams").where({rtmpKey:req.body.name});
+  if(streams.length==0)
+    return res.send(404);
+
+
+
   res.json("ok")
 });
 
