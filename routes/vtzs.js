f7a257aca073578dffbf21ea0e7ff8d0c5d171bd
radoneje <53825282+radoneje@users.noreply.github.com>
2022-11-17 18:22:58 +0300

    ddd

--- routes/vtzs.js
+++ routes/vtzs.js
@@ -16,10 +16,13 @@ function startRestreamToHLS(key, lang, req){
   let params=[ "-re", "-i", "rtmp://localhost/live/"+key, "-c:v", "copy","-c:a", "aac", "-af", "pan=mono|c0=c"+(lang=="ru"?0:1), "-f", "flv", "rtmp://localhost/hls/"+key+lang ]
   //let params=["-m","/var/restream",key, lang, (lang=="ru"?0:1)]
   let stream = spawn("ffmpeg", params , {detached: true, stdio: 'ignore'});
-  //stream.on("close", async (code) => {
-  //  console.log(`ffmpeg close om ${key} ${lang}`);
-  //});
-  stream.unref();
+  stream.on("close", async (code) => {
+    console.log(`ffmpeg close om ${key} ${lang}`);
+  });
+  stream.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+  });
+  //stream.unref();
 }
 
 module.exports = router;
