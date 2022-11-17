c57527890ec12493044253d3dd2a22d900537d7c
radoneje <53825282+radoneje@users.noreply.github.com>
2022-11-17 18:20:41 +0300

    ddd

--- routes/vtzs.js
+++ routes/vtzs.js
@@ -7,12 +7,12 @@ router.post('/startStream', async function(req, res, next) {
 
   setTimeout(()=>{
 
-    startRestreamToHLS(req.body.name,"ru", streams[0].id, req);
+    startRestreamToHLS(req.body.name,"ru", req);
   },500)
   res.json(1)
 });
 
-function startRestreamToHLS(key, lang, streamid, req){
+function startRestreamToHLS(key, lang, req){
   let params=[ "-re", "-i", "rtmp://localhost/live/"+key, "-c:v", "copy","-c:a", "aac", "-af", "pan=mono|c0=c"+(lang=="ru"?0:1), "-f", "flv", "rtmp://localhost/hls/"+key+lang ]
   //let params=["-m","/var/restream",key, lang, (lang=="ru"?0:1)]
   let stream = spawn("ffmpeg", params , {detached: true, stdio: 'ignore'});
